c49b2a1d49ef65bb92dea108883012de64a6ff97
max.stargardt@uni-oldenburg.de <max.stargardt@uni-oldenburg.de>
2020-10-23 15:25:43 +0200

    Strukturierung des Projekts

--- main/main.py
+++ main/main.py
@@ -1,40 +1,20 @@
-# Snake Tutorial Python
+import asyncio
+import json
+import websockets
 
-import random
-
-import pygame
-
-from main.gameGraphic.playgroundGraphic import playgroundGraphic
-
-
-def createRandomColor():
-    """Creates a random RGB (int, int int) color"""
-    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
-
-
-def createRandomPosition(rows: int, column: int):
-    """Creates a random Position in within the given :param rows and :param column and returns it as (int, int)"""
-    x = random.randrange(rows)
-    y = random.randrange(column)
-    return x, y
-
-
-def main():
-    global pixelWidth, pixelHeight, rows, columns
-
-    pixelWidth = 500
-    pixelHeight = 500
-
-    rows = 20  # int(input("Anzahl der Zeilen eingeben: "))
-    columns = 30  # int(input("Anzahl der Spalten eingeben: "))
-
-    playfield = playgroundGraphic.Playfield(pixelWidth, pixelHeight, rows, columns)
-
-    playfield.updateWindow()
-
-    pygame.time.delay(10000)
-
+def __init__():
     pass
 
 
-main()
+async def play():
+    url = "wss://msoll.de/spe_ed"
+    key = "72ILGT3YVIW5DV2UR3L5E6VCMFB6TJPR6LAX2ZLGMYGRQSVTW2C4G4E2"
+
+    async with websockets.connect(f"{url}?key={key}") as websocket:
+        print("Waiting for initial state...", flush=True)
+        while True:
+            state_json = await websocket.recv()
+            state = json.loads(state_json)
+            print("<", state)
+
+asyncio.get_event_loop().run_until_complete(play())
