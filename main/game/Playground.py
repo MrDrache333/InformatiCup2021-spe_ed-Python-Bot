5a399474dbecb9e07eee1182a06637966dac1071
max.stargardt@uni-oldenburg.de <max.stargardt@uni-oldenburg.de>
2020-10-30 11:24:05 +0100

    *Player Funktionen hinzugefügt *Playground movePlayer Funktion hinzugefügt

--- main/game/Playground.py
+++ main/game/Playground.py
@@ -1,14 +1,52 @@
+from game.player.DirectionOfLooking import DirectionOfLooking
 from game.player.Player import Player
+
 
 class Playground(object):
 
-        def __init__(self, coordinateSystem, players):
-            self.coordinateSystem = coordinateSystem
-            self.players = players
+    def __init__(self, coordinateSystem, players):
+        self.coordinateSystem = coordinateSystem
+        self.players = players
 
-        def lookInInStraightLine(self, player, direction, howFarToLook) -> bool: #TODO make direction ENUM
-            """Returns whether something is within the range :param howFarToLook in the direction defined by :param
+    def lookInInStraightLine(self, player, direction, howFarToLook) -> bool:  # TODO make direction ENUM
+        """Returns whether something is within the range :param howFarToLook in the direction defined by :param
             direction """
-            pass
+        pass
 
+    def movePlayer(self):
+        for player in self.players:
+            # read speed,direction, position and activity of player
+            # determine whether player would colide with a wall
+            # move player to new position
+            # create walls where the player moved
+            # move on to the next player
 
+            if (player.active):
+                xCoordinateOfPlayer = player.x
+                yCoodinateOfPlayer = player.y
+                speedOfPlayer = player.speed
+                directionOfPlayer = player.directionOfLooking
+
+                if (directionOfPlayer == DirectionOfLooking.LEFT):
+                    xCoordinateOfPlayer -= speedOfPlayer
+                elif (directionOfPlayer == DirectionOfLooking.UP):
+                    yCoodinateOfPlayer -= speedOfPlayer
+                elif (directionOfPlayer == DirectionOfLooking.RIGHT):
+                    xCoordinateOfPlayer += speedOfPlayer
+                elif (directionOfPlayer == DirectionOfLooking.DOWN):
+                    yCoodinateOfPlayer += speedOfPlayer
+
+                # determine whether player would collide with a wall
+                # determine whether coordinates are within coordinatesystem
+                if (xCoordinateOfPlayer <= len(self.coordinateSystem[0]) and yCoodinateOfPlayer <= len(
+                        self.coordinateSystem)):
+                    if (self.coordinateSystem[xCoordinateOfPlayer][yCoodinateOfPlayer] == 0):
+                        # Player did not collide with wall
+
+                        # update player coords
+                        player.x = xCoordinateOfPlayer
+                        player.y = yCoodinateOfPlayer
+
+                        # update coordinate system
+                        self.coordinateSystem[xCoordinateOfPlayer][yCoodinateOfPlayer] = player.id
+                        # TODO draw walls where the player moved
--- main/game/player/Player.py
+++ main/game/player/Player.py
@@ -1,8 +1,41 @@
+from game.player.DirectionOfLooking import DirectionOfLooking
+import logging, sys
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+
 class Player(object):
     def __init__(self, id: int, x: int, y: int, directionOfLooking: str, active: bool, speed: int):
         self.id = id
         self.x = x
         self.y = y
-        self.directionOfLooking = directionOfLooking
+        self.directionOfLooking = DirectionOfLooking[directionOfLooking.upper()]
         self.active = active
         self.speed = speed
+
+    def turnDirectionOfLooking(self, directionOfLooking: DirectionOfLooking):
+        if(directionOfLooking%2 == self.directionOfLooking%2) :
+            logging.debug('Cant change direction, reason: Input direction is in opposite or same direction as previous one ')
+        else :
+            self.directionOfLooking = directionOfLooking
+
+    def speedUp(self):
+        if(self.speed == 10) :
+            logging.debug('Cant accelerate, reason: I Am Speed! (Speed = 10)')
+        else :
+            self.speed += 1
+
+    def speedDown(self):
+        if(self.speed == 1) :
+            logging.debug('Cant decelerate, reason: Don\'t stop me now! (Speed =1 )')
+        else :
+            self.speed -= 1
+
+    def updatePlayer(self, id: int, x: int, y: int, directionOfLooking: DirectionOfLooking, active: bool, speed: int):
+        if(id != self.id) :
+            logging.debug('No matching ID of player')
+        else :
+            self.x = x
+            self.y = y
+            self.directionOfLooking = directionOfLooking
+            self.active = active
+            self.speed = speed
